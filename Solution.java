35605e6795fdb17127f8c6c5b72b0491068271b7
KeertiSaikrishna <31760338+KeertiSaikrishna@users.noreply.github.com>
2018-09-07 12:35:10 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,125 +1,30 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
     public String solution(String S) {
-        String[] arrOfStr = S.split("\n", -2);
-        int musicStats = 0;
-        int imageStats = 0;
-        int movieStats = 0;
-        int otherStats = 0;
-        for (String str : arrOfStr) {
-            musicStats += music_size(str);
-            imageStats += imgSize(str);
-            movieStats += mvSize(str);
-            otherStats += othersSize(str);
+
+        String[] arrOfNumbers = S.split("-| ");
+                                        // removind '-' and ' ' from string and storing it in arrOfNumbers
+        String numbersOnly = String.join("", arrOfNumbers);
+                                        //creating a String of number without any delimiters
+        List<String> listOfNumbers = new ArrayList<String>();
+
+        int indexOfJoinedString = 0;
+        while (indexOfJoinedString < numbersOnly.length()) {
+            if ((numbersOnly.length() - indexOfJoinedString) > 4 || indexOfJoinedString + 3 == numbersOnly.length()) {
+                listOfNumbers.add(numbersOnly.substring(indexOfJoinedString,
+                        Math.min(indexOfJoinedString + 3, numbersOnly.length())));
+                indexOfJoinedString += 3;
+            } else {
+                listOfNumbers.add(numbersOnly.substring(indexOfJoinedString,
+                        Math.min(indexOfJoinedString + 2, numbersOnly.length())));
+                indexOfJoinedString += 2;
+            }
         }
-        return ("music " + musicStats + "b" + "\nimages " + imageStats +
-                "b" + "\nmovies " + movieStats + "b" + "\nothers " + otherStats + "b");
-    }
-
-    public static int music_size(String s) {
-        Pattern p1 = Pattern.compile(".mp3");
-        Pattern p2 = Pattern.compile(".aac");
-        Pattern p3 = Pattern.compile(".flac");
-
-        Matcher m1 = p1.matcher(s);
-        Matcher m2 = p2.matcher(s);
-        Matcher m3 = p3.matcher(s);
-        String[] arrToSeparteNameAndSize = s.split(" ", 2);
-        String[] arrToFindSize;
-
-        if (m1.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m2.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m3.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else {
-            return 0;
-        }
-    }
-
-    public static int imgSize(String s) {
-        Pattern p1 = Pattern.compile(".jpg");
-        Pattern p2 = Pattern.compile(".bmp");
-        Pattern p3 = Pattern.compile(".gif");
-
-        Matcher m1 = p1.matcher(s);
-        Matcher m2 = p2.matcher(s);
-        Matcher m3 = p3.matcher(s);
-        String[] arrToSeparteNameAndSize = s.split(" ", 2);
-        String[] arrToFindSize;
-
-        if (m1.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m2.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m3.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else {
-            return 0;
-        }
-    }
-
-    public static int mvSize(String s) {
-        Pattern p1 = Pattern.compile(".mp4");
-        Pattern p2 = Pattern.compile(".avi");
-        Pattern p3 = Pattern.compile(".mkv");
-
-        Matcher m1 = p1.matcher(s);
-        Matcher m2 = p2.matcher(s);
-        Matcher m3 = p3.matcher(s);
-        String[] arrToSeparteNameAndSize = s.split(" ", 2);
-        String[] arrToFindSize;
-
-        if (m1.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m2.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m3.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else {
-            return 0;
-        }
-    }
-
-    public static int othersSize(String s) {
-        Pattern p1 = Pattern.compile(".txt");
-        Pattern p2 = Pattern.compile(".exe");
-        Pattern p3 = Pattern.compile(".zip");
-        Pattern p4 = Pattern.compile(".7z");
-
-        Matcher m1 = p1.matcher(s);
-        Matcher m2 = p2.matcher(s);
-        Matcher m3 = p3.matcher(s);
-        Matcher m4 = p4.matcher(s);
-        String[] arrToSeparteNameAndSize = s.split(" ", 2);
-        String[] arrToFindSize;
-
-        if (m1.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m2.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m3.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else if (m4.find()) {
-            arrToFindSize = arrToSeparteNameAndSize[1].split("((?<=[a-zA-Z])(?=[0-9]))|((?<=[0-9])(?=[a-zA-Z]))", -2);
-            return (Integer.valueOf(arrToFindSize[0]));
-        } else {
-            return 0;
-        }
+                                        //deviding the numbers into 3 or 2 groups and storing them in listOfNumbers
+        String formattedNumber = String.join("-", listOfNumbers);
+                                        //creating the formattedNumber using delimiter "-"
+        return formattedNumber;
     }
 }
--- TestSolution.java
+++ TestSolution.java
@@ -1,9 +1,10 @@
+
 public class TestSolution {
     public static void main(String[] args) {
-        String S = "my.song.mp3 11b\nngreatSong.flac 1000b\nnot3.txt 5b\nvideo.mp4 200b\ngame.exe 100b\nmov!e.mkv 10000b";
-
-        Solution obj = new Solution();
-        String stats = obj.solution(S);
-        System.out.println(stats);
+        String S = "555372654";
+        String formattedNumber;
+        Solution test1 = new Solution();
+        formattedNumber = test1.solution(S);
+        System.out.println(formattedNumber);
     }
 }
